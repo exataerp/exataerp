d4f1030e260a7d997f3dca66f135353e7bf06bb6
exataerp <contatoexataerp@yahoo.com>
2026-07-28 08:04:17 -0300

    Create page.tsx

---
+++ app/admin/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { MasterTab } from "@/components/master-tab"
+
+export default function AdminPage() {
+  return (
+    <div className="min-h-screen bg-background px-4 py-8 lg:px-10">
+      <div className="max-w-6xl mx-auto">
+        <MasterTab />
+      </div>
+    </div>
+  )
+}
